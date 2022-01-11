5a7e367b183c7353451db38f5eb96476b7a8bb52
Cibbi <12004047+Cibbi@users.noreply.github.com>
2022-01-12 00:43:47 +0100

    Fixed link

--- Editor/TSRConstants.cs
+++ Editor/TSRConstants.cs
@@ -3,7 +3,7 @@
     public static class TSRConstants
     {
         public const string TSR_VERSION = "Toony Standard RE:Build Test Build 6";
-        public const string GITHUB_LINK = "https://github.vrlabs.dev";
+        public const string GITHUB_LINK = "https://github.com/VRLabs/Toony-Standard-Rebuild";
         public const string PATREON_LINK = "https://patreon.vrlabs.dev";
         public const string DISCORD_LINK = "https://discord.vrlabs.dev";
     }
